ef88e8cf788eef1fc25f21b15d54b3da553a9c1d
Ogaraku Justine <justineugoogaraku@gmail.com>
2024-11-12 22:59:41 +0100

    chore: set assets files

---
+++ lib/util/assets.dart
@@ -0,0 +1,16 @@
+class AssetsImages{
+  static String profile = 'assets/images/profile.png';
+}
+
+class AssetsIcons{
+  static String btuUsd = 'assets/svgs/btc_usd.svg';
+  static String candleIcon = 'assets/svgs/candle_icon.svg';
+  static String dropdownIcon = 'assets/svgs/dropdown_icon.svg';
+  static String expandIcon = 'assets/svgs/expand_icon.svg';
+  static String globeIcon = 'assets/svgs/globe_icon.svg';
+  static String indicator1 = 'assets/svgs/indicator_1.svg';
+  static String indicator2 = 'assets/svgs/indicator_2.svg';
+  static String indicator3 = 'assets/svgs/indicator_3.svg';
+  static String logo = 'assets/svgs/logo.svg';
+  static String menuIcon = 'assets/svgs/menu_icon.svg';
+}
